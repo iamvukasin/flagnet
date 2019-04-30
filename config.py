7841bbf3123d45199d5d425e363ef50113bf8c95
Vukašin Manojlović <iamvukasin@gmail.com>
2019-04-30 15:01:51 +0200

    Fix downloader to download the dataset in parallel

--- config.py
+++ config.py
@@ -7,3 +7,6 @@ CACHE_FOLDER = os.path.join(PROJECT_ROOT, '.cache')
 
 MIN_IMAGE_SIZE = 416, 416
 SCREENSHOT_IMAGE_SIZE = 800, 600
+
+# number of parallel workers when downloading the dataset
+NUM_DOWNLOAD_WORKERS = 8
--- download/download_dataset.py
+++ download/download_dataset.py
@@ -1,21 +1,53 @@
 import glob
+from collections import namedtuple
+from multiprocessing.pool import ThreadPool
 
 import ruamel.yaml as yaml
 
 import config
 from download.url_image_downloader import UrlImageDownloader
 
-_DOWNLOADERS = {downloader.type: downloader for downloader in (UrlImageDownloader,)}
+PhotoItem = namedtuple('PhotoItem', ['downloader', 'download_url', 'path'])
+
+DOWNLOADERS = {downloader.type: downloader for downloader in (UrlImageDownloader,)}
+
+
+def _download_single_photo(item: PhotoItem):
+    """
+    Downloads a single photo with the given downloader, the URL to download
+    from and the local path where to store the downloaded photo.
+
+    :param item: a tuple which contains downloader name, download URL and
+    local path
+    """
+
+    downloader = DOWNLOADERS[item.downloader](item.download_url, item.path)
+    downloader.download()
+
 
 if __name__ == '__main__':
+    photo_items = []
+
     for credits in glob.glob(f'{config.DATASET_FOLDER}/*/credits.yml'):
-        # read all photos for single class
+        # read all photos for a single country
         with open(credits) as yaml_file:
             data = yaml.load(yaml_file, Loader=yaml.Loader)
 
-        # download all photos
         for photo_data in data['photos']:
+            downloader_type = photo_data['downloader']
             download_url = photo_data['download_url']
             path = credits.replace('credits.yml', photo_data['filename'])
-            downloader = _DOWNLOADERS[photo_data['downloader']](download_url, path)
-            downloader.download()
+
+            item = PhotoItem(
+                downloader=downloader_type,
+                download_url=download_url,
+                path=path
+            )
+            photo_items.append(item)
+
+    # download photos in parallel
+    results = ThreadPool(config.NUM_DOWNLOAD_WORKERS).imap_unordered(_download_single_photo, photo_items)
+
+    # wait all downloads to finish
+    for _ in results:
+        pass
